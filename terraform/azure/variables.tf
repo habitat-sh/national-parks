1b2f0e415d5fcb5450df033770d7ee189233bc04
Scott Ford <smford22@gmail.com>
2018-01-30 06:54:24 -0800

    removing comments

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -1,64 +1,51 @@
-# Azure Region
 variable "azure_region" {
   default = "westus2"
 }
 
-# Public SSH key path
 variable "azure_public_key_path" {
   default = "/path/to/ssh/key"
 }
 
-# Private SSH key path
 variable "azure_private_key_path" {
   default = "/path/to/ssh/key"
 }
 
-# Azure image user for SSH
 variable "azure_image_user" {
   default = "azureuser"
 }
 
-# Password for image user
 variable "azure_image_password" {
   default = "Azur3pa$$word"
 }
 
-# Azure subscriber id
 variable "azure_sub_id" {
   default = "xxxxxxx-xxxx-xxxx-xxxxxxxxxx"
 }
 
-# Azure tenant ID
 variable "azure_tenant_id" {
   default = "xxxxxxx-xxxx-xxxx-xxxxxxxxxx"
 }
 
-# Application Name
 variable "application" {
   default = "nationalparks"
 }
 
-# Habitat origin to use
 variable "habitat_origin" {
-  default = "scottford"
+  default = "core"
 }
 
-# Habitat build url
 variable "bldr_url" {
   default = "https://bldr.habitat.sh"
 }
 
-# Habitat release channel
 variable "release_channel" {
   default = "stable"
 }
 
-# Habitat supervior group name
 variable "group" {
   default = "dev"
 }
 
-# Habitat update strategy 
 variable "update_strategy" {
   default = "at-once"
 }
