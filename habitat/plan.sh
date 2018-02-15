4734087bf98a7bfa64f81c40ed6673a4589ca0b7
lhasadreams <lhasadreams@users.noreply.github.com>
2018-02-15 12:50:19 +0000

    Update plan.sh

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,7 +1,7 @@
 pkg_name=national-parks
 pkg_description="A sample JavaEE Web app deployed in the Tomcat8 package"
 pkg_origin=national
-pkg_version=6.4.0
+pkg_version=6.5.0
 pkg_maintainer="Bill Meyer <bill@chef.io>"
 pkg_license=('Apache-2.0')
 pkg_deps=(core/tomcat8 core/jre8)
