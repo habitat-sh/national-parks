0f34a20810f2833778491ede639dd1e049955c56
Scott Ford <smford22@gmail.com>
2018-04-25 11:17:56 -0700

    adding latest changes

--- terraform/azure/outputs.tf
+++ terraform/azure/outputs.tf
@@ -1,11 +1,3 @@
-output "pip-initial_peer" {
-  value = "${azurerm_public_ip.pip.0.ip_address}",
-}
-
-output "pip-mongodb" {
-  value = "${azurerm_public_ip.pip.1.ip_address}",
-}
-
-output "pip-np_app" {
-  value = "${azurerm_public_ip.pip.2.ip_address}",
+output "instance_ips" {
+  value = ["${azurerm_public_ip.pip.*.ip_address}"]
 }
