d803ed2f4ecc6b40ed9b8657ff788ec2fbf02343
Jeff Mery <jeff@merys.net>
2018-06-07 13:30:03 -0500

    Cleanup unused template resources

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -253,28 +253,3 @@ resource "aws_instance" "national-parks" {
     }
   }
 }
-
-////////////////////////////////
-// Templates
-
-
-#data "template_file" "initial_peer" {
-#  template = "${file("${path.module}/../templates/hab-sup.service")}"
-#
-#  vars {
-#    flags = "--auto-update --listen-gossip 0.0.0.0:9638 --listen-http 0.0.0.0:9631 --permanent-peer"
-#  }
-#}
-#
-#data "template_file" "sup_service" {
-#  template = "${file("${path.module}/../templates/hab-sup.service")}"
-#
-#  vars {
-#    flags = "--auto-update --peer ${aws_instance.initial-peer.private_ip} --listen-gossip 0.0.0.0:9638 --listen-http 0.0.0.0:9631"
-#  }
-#}
-#
-#data "template_file" "install_hab" {
-#  template = "${file("${path.module}/../templates/install-hab.sh")}"
-#}
-
